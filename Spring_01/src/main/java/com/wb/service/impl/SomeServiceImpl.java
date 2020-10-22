b12c34da48c962bbf2bb0e849e779dc438a2c2f3
wblueberry <wblueberry@126.com>
2020-10-22 22:39:13 +0800

    Initial commit

---
+++ Spring_01/src/main/java/com/wb/service/impl/SomeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.wb.service.impl;
+
+import com.wb.service.SomeService;
+
+public class SomeServiceImpl implements SomeService {
+    @Override
+    public void doSome() {
+        System.out.println("执行了方法");
+    }
+}
---
+++ Spring_01/src/test/java/com/wb/MyTest.java
@@ -0,0 +1,10 @@
+package com.wb;
+
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test01(){
+
+    }
+}
